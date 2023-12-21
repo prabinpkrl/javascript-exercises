4a7e0dda22bec1739822427cab0f923ca1839520
Prabin Pokhrel <prabinpokhrel7722@gmail.com>
2023-12-21 16:56:24 +0545

    reversing a string by 3 method

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,20 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    // // using loop
+    // let reversestring='';
+    // for(var i=string.length-1; i>=0; i--){
+    //     reversestring+=string[i];
+    // }
+    // return reversestring;
+
+
+    //    using build in function 
+    let splitstring=string.split('');
+    let newarray=splitstring.reverse('');
+    let reverseString=newarray.join('');
+    return reverseString;
+
+    // chaining
+    // return string.split("").reverse().join("");
 
 };
 
