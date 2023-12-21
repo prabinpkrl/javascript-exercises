f92cb863935d6b25b181ea8f7e5728b62a647010
Prabin Pokhrel <prabinpokhrel7722@gmail.com>
2023-12-21 16:20:47 +0545

    test pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    if (times < 0) return "ERROR";    
+    let repeatString='';
+    for(let i=1; i<=times; i++){
+            repeatString+=string;
+        }
+    return repeatString;    
 };
 
 // Do not edit below this line
