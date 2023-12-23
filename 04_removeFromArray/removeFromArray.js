3f105ecfcc4d266d0919af92cccf32b3c1db1340
Prabin Pokhrel <prabinpokhrel7722@gmail.com>
2023-12-23 21:55:27 +0545

    learn about arrow function and solves it

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...arg) {
+   
+   //using foreach function
+    // const newArray=[];
 
+    // array.forEach(element => {
+    //         if(!arg.includes(element)){
+    //             newArray.push(element);
+    //         }
+            
+    //     });
+    //     return newArray;
+
+// using flter() function
+    return array.filter(val => !arg.includes(val));
+    
 };
 
 // Do not edit below this line
