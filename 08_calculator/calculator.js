b4adfd4961ff1bc3ee8df956609ead70e51b4157
Prabin Pokhrel <prabinpokhrel7722@gmail.com>
2023-12-26 13:00:26 +0545

    solved calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,55 @@
-const add = function() {
+const add = function(num1, num2) {
+  if(!Number.isInteger(num1) || !Number.isInteger(num2) ){
+    return 'ERROR';
+}
+    return num1 + num2 ;
 	
 };
 
-const subtract = function() {
+const subtract = function(num1, num2) {
+  if(!Number.isInteger(num1) || !Number.isInteger(num2) ){
+    return 'ERROR';
+}
+    return num1 - num2 ;
 	
 };
 
-const sum = function() {
+const sum = function(num) {
+  num= typeof num != 'undefined'? num: 0;
+  
+  var sumOfNumber=0;
+  for (let i=0; i< num.length; i++){
+  	sumOfNumber += num[i];
+  }
+  return sumOfNumber;
+
+};
+	
+
+const multiply = function(num) {
+  num= typeof num != 'undefined'? num: 0;
+  
+  var multiplyOfNumber=1;
+  for (let i=0; i<= num.length-1; i++){
+  	multiplyOfNumber *= num[i];
+  }
+  return multiplyOfNumber;
+
+};
+
+const power = function(base, power) {
+    return Math.pow(base, power);
 	
 };
 
-const multiply = function() {
+const factorial = function(num) {
+  if(num === 0 || num === 1){
+    return 1;
+  }
+  else{
+    return num *factorial( num-1);
+  }
 
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
 };
 
 // Do not edit below this line
