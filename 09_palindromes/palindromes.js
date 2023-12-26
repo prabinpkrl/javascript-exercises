2539598c2e3b5168647340a3dda754fa6e52c7ea
Prabin Pokhrel <prabinpokhrel7722@gmail.com>
2023-12-26 13:50:57 +0545

    solved palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+  // method 
+    let palindromesString = string.toLowerCase().replace(/[^a-z0-9]/g, "")
+    let resultplaindrome =palindromesString.split('').reverse().join('');
+    
+ // here we have return true or false by ourself as we are using '!==' or '===' expression.
+    if(palindromesString !== resultplaindrome){
+        return false;
+    }
+    
+    return true;
+
+    // OR directly method
+        //     const processedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+// here '==' means it is boolean expression itself so it return true or false.
+        //   return processedString.split("").reverse().join("") == processedString; 
 
 };
 
